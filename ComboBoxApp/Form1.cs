0513ae4db21f6632fe84ab8bfac3d0cf9c5af48e
SebastianRestrepoA <sebastianrestrepoa@example.com>
2022-03-02 21:29:52 -0500

    upload sql and comboBox apps

---
+++ ComboBoxApp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = "El departamento seleccionado fue: " + comboBox1.Text;
+        }
+    }
+}
